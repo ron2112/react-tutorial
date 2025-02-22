2109ee6622b7ae309c54e5046ba5ea0edc6fc822
ron2112 <ghoshabhra5@gmail.com>
2025-02-22 18:45:51 +0530

    tailwind setup + props

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  let myArr = [1,2,3,4]
+
+  return (
+    <>
+      <h1 className='text-3xl bg-green-500 p-3 rounded-md'>Vite with Tailwind</h1>
+      <Card username="Abhra" myArr={myArr} />
+      <Card />
+      <Card />
+    </>
+  )
+}
+
+export default App
